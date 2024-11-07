cc150d249c2ba314ddd1f9f8f6919071a6df02e8
anisa <anisamara777@gmail.com>
2024-11-07 13:46:26 +0100

    Exercice 3 : Rectification de echanger

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -266,13 +266,13 @@ class ListeSimpleTest {
         listeATester.ajout(5);
         listeATester.ajout(4);
         Noeud r1 = listeATester.tete;
-        listeATester.ajout(3);
-        listeATester.ajout(4);
         Noeud r2 = listeATester.tete;
+        listeATester.ajout(3);
+        listeATester.ajout(2);
         listeATester.ajout(1);
-        assertEquals("ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(4), Noeud(5))", listeATester.toString());
+        assertEquals("ListeSimple(Noeud(1), Noeud(2), Noeud(3), Noeud(4), Noeud(5))", listeATester.toString());
         listeATester.echanger(r1, r2);
-        assertEquals("ListeSimple(Noeud(1), Noeud(4), Noeud(3), Noeud(4), Noeud(5))", listeATester.toString());
+        assertEquals("ListeSimple(Noeud(1), Noeud(2), Noeud(3), Noeud(4), Noeud(5))", listeATester.toString());
     }
 
     @Test
