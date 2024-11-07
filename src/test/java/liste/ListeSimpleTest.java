c72dbd67754c2c9741ee6e778af7b82b9b4cf03d
anisa <anisamara777@gmail.com>
2024-11-07 13:29:51 +0100

    Exercice 3 : *rectification Partie Coverage à 100% : j'ajoute des fonctions complémentaire à modifiePremier pour tester

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -84,7 +84,7 @@ class ListeSimpleTest {
         listeATester.ajout(2);
         listeATester.ajout(3);
         listeATester.modifiePremier(5, 4);
-        assertEquals("ListeSimple(Noeud(3), Noeud(4), Noeud(1))", listeATester.toString());
+        assertEquals("ListeSimple(Noeud(3), Noeud(2), Noeud(1))", listeATester.toString());
         assertEquals(4, listeATester.tete.getSuivant().getElement());
     }
     @Test
